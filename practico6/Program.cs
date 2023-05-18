90bf0104d784e40f7fcfd57080dcf377607b0536
slakku <bananuya3@gmail.com>
2023-05-18 19:16:07 -0300

    primer ejercicio

---
+++ practico6/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ingrese un numero: ");
+string? g = Console.ReadLine();
+if (Single.TryParse(g, out float num))
+{
+    float numero = Single.Parse(g);
+    if (numero > 0)
+    {
+        float inverted = 1 / numero;
+        Console.WriteLine("El numero que se obtuvo es: " + inverted);
+    }
+    else
+    {
+        Console.WriteLine("El numero ingresado no es valido.");
+    }
+}
+else
+{
+    Console.WriteLine("No ha ingresado un numero.");
+}
