40f068f1b508e1a2922e914f3639df40f81b9717
slacku <bananuya3@gmail.com>
2023-05-26 13:50:44 -0300

    CalculadoraV2 branch

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,17 +1,62 @@
-﻿Console.WriteLine("Ingrese un numero: ");
-string? g = Console.ReadLine();
-if (Single.TryParse(g, out float num))
+﻿Console.Write("|--------------------------------------------------|\n");
+Console.Write("|                  CALCULADORA V2                  |\n");
+Console.Write("|--------------------------------------------------|\n");
+Console.Write("Ingrese un numero: ");
+string? input = Console.ReadLine();
+if (Single.TryParse(input, out float n1))
 {
-    float numero = Single.Parse(g);
+    float numero = Single.Parse(input);
+    Console.WriteLine("El valor absoluto de " + numero + ": " + Single.Abs(numero));
+    Console.WriteLine("El cuadrado de " + numero + ": " + numero * numero);
     if (numero > 0)
     {
-        float inverted = 1 / numero;
-        Console.WriteLine("El numero que se obtuvo es: " + inverted);
+        Console.WriteLine("La raiz cuadrada de " + numero + ": " + Single.Sqrt(numero));
     }
     else
     {
-        Console.WriteLine("El numero ingresado no es valido.");
+        Console.WriteLine("La raiz cuadrada del numero ingresado no es un numero real");
     }
+    Console.WriteLine("El seno de " + numero + ": " + Single.Sin(numero));
+    Console.WriteLine("El coseno de " + numero + ": " + Single.Cos(numero));
+    var numString = numero.ToString();
+    string[] arrNumString = numString.Split(',');
+    Console.WriteLine("La parte entera de " + numero + ": " + arrNumString[0]);
+
+    Console.Write("Ingrese otro numero: ");
+    string? secondInput = Console.ReadLine();
+    if (Single.TryParse(secondInput, out float n2))
+    {
+        float num1 = Single.Parse(secondInput);
+        Console.Write("Ingrese otro numero: ");
+        string? thirdInput = Console.ReadLine();
+        if (Single.TryParse(thirdInput, out float n3))
+        {
+            float num2 = Single.Parse(thirdInput);
+            if (num1 > num2)
+            {
+                Console.WriteLine("El maximo entre " + num1 + " y " + num2 + " es " + num1);
+                Console.WriteLine("El minimo entre " + num1 + " y " + num2 + " es " + num2);
+            }
+            else
+            {
+                Console.WriteLine("El maximo entre " + num1 + " y " + num2 + " es " + num2);
+                Console.WriteLine("El minimo entre " + num1 + " y " + num2 + " es " + num1);
+            }
+
+
+
+        }
+        else
+        {
+            Console.WriteLine("No ha ingresado un numero.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("No ha ingresado un numero.");
+    }
+
+
 }
 else
 {
